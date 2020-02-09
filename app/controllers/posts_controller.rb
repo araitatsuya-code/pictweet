2d70b28fc9484f88913cd64ffd9818f6c36b1b36
araitatsuya-code <58362112+araitatsuya-code@users.noreply.github.com>
2020-02-09 09:10:25 +0900

    add devise

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
+
   def index
     @posts = Post.all
   end
@@ -6,13 +10,38 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
-end
 
-def create
-  Post.create(post_params)
-end
+  def create
+    Post.create(post_params)
+  end
 
-private
-def post_params
-  params.permit(:name, :image, :text)
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
+  def show
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id]) 
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
